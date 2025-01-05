645a6caa870a96290b527694a7cb00ff36bb5158
codvary <codvary@gmail.com>
2025-01-05 19:24:00 +0530

    adding module implementation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,8 @@
 import { Component } from '@angular/core';
-import { HeaderComponent } from "./component/header/header.component";
-import { FormComponent } from './component/form/form.component';
-import { ResultComponent } from './component/result/result.component';
 
 @Component({
   selector: 'app-root',
-  imports: [HeaderComponent, FormComponent, ResultComponent],
+  standalone: false,
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
